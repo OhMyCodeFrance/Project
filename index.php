881db66f4647448862725f0b29e06a836161bbb5
OhMyCodeNews <ohmycodehaguenau@gmail.com>
2016-12-02 00:40:39 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,43 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <title>Titre de la page</title>
-  <link rel="stylesheet" href="assets/css/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <script src="assets/js/main.js"></script>
-</head>
-<body>
-<div class="content">
-    
-</div>
-</body>
-</html>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Projet - Nuit de l'Info 2016</title>
+
+  <!-- Bootstrap -->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+    </head>
+    <body>
+      <section id="accueil">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 wrapper-header">
+              <header>
+                <h1>BIENVENUE CHEZ VOUS</h1>
+                <div class="access">
+                <a href="content.php" class="btn btn-ask" data-transition="slide" data-direction="reverse">Demander de l'aide</a>
+                <a href="help.php" class="btn btn-help">Proposer de l'aide</a>
+                </div>
+              </header>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="js/bootstrap.min.js"></script>
+    </body>
+    </html>
