d9e29120dce20d95805b991cc4736ae6bc659adf
Masi <masimojoke@gmail.com>
2016-12-02 00:30:56 +0100

    Ajout du système de QR Code Via un exemple

--- assets/views/home.php
+++ assets/views/home.php
@@ -1,6 +1,5 @@
 <p>Home.php</p>
 
-<?php include(''); ?>
 <button id="add_post">
     Ajouter un post
 </button>
@@ -8,3 +7,5 @@
 <button id="display_posts">
     Afficher les posts
 </button>
+
+<img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http%3A%2F%2Fwww.google.com%2F&choe=UTF-8" title="Link to Google.com" />
